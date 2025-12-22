744bb2e661ab44a44f1cb15b55dd15bf4be72511
vasanthkiaq <vasanthkiaq.in@gmail.com>
2025-12-22 10:55:44 +0530

    4thcommit

--- platform-service/src/app.js
+++ platform-service/src/app.js
@@ -1,10 +1,19 @@
 import express from "express";
-const app = express();
+import cors from "cors";
+import morgan from "morgan";
+import userRoutes from './app/routes/user.routes.js';
+import errorHandler from './core/middleware/errorHandler.js';
 
+const app = express();
+app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
+
+app.use('/users', userRoutes);
 
 app.get("/", (req, res) => {
-  res.send(" Auth Service is running successfully!");
+  res.send(" Platform Service is running successfully!");
 });
 
+app.use(errorHandler);
 export default app;
